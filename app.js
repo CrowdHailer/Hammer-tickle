2125246e07544e150b495cf6a3407add6a35d937
CrowdHailer <peterhsaxton@gmail.com>
2013-12-12 11:22:38 +0000

    Extends list on event

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var myApp = {
 	hammerEvents: "touch release tap hold doubletap drag dragstart dragend dragup dragdown dragleft dragright swipe swipeup swipedown swipeleft swiperight transform transformstart transformend rotate pinch pinchin pinchout",
 	other: "click dblclick mousedown",
 	init: function (){
+		this.output = document.getElementById('events');
 		this.hammertime = Hammer(document).on(this.hammerEvents, function(evt){
 			console.log(evt.type);
+			myApp.addToList(evt);
 		});
+	},
+	addToList: function (evt) {
+		this.output.innerHTML += '<li> woooo </li>';
 	}
 };
 myApp.init();
